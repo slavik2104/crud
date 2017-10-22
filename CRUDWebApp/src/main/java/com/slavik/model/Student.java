a7b2502b4eabb91b442230d7d046b73eeb2c8d9f
slavik2104 <slavik210485@gmail.com>
2017-10-22 15:47:16 +0300

    CrudWebApp

---
+++ CRUDWebApp/src/main/java/com/slavik/model/Student.java
@@ -0,0 +1,74 @@
+package com.slavik.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+	
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int studentId;
+	@Column
+	private String firstname;
+	@Column
+	private String lastname;
+	@Column
+	private int yearLevel;
+	
+	public Student() {}
+
+	public Student(int studentId, String firstname, String lastname, int yearLevel) {
+		this.studentId = studentId;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.yearLevel = yearLevel;
+	}
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public int getYearLevel() {
+		return yearLevel;
+	}
+
+	public void setYearLevel(int yearLevel) {
+		this.yearLevel = yearLevel;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [studentId=" + studentId + ", firstname=" + firstname + ", lastname=" + lastname
+				+ ", yearLevel=" + yearLevel + "]";
+	}
+	
+	
+	
+	
+
+}
---
+++ CRUDWebApp/src/main/java/com/slavik/service/StudentService.java
@@ -0,0 +1,15 @@
+package com.slavik.service;
+
+import java.util.List;
+
+import com.slavik.model.Student;
+
+public interface StudentService {
+	
+	public void add(Student student);
+	public void edit(Student student);
+	public void delete(int studentId);
+	public Student getStudent(int studentId);
+	public List getAllStudent();
+
+}
